66f02377435c87febd9d2987d83ffafc8c9c50d9
Khalid <khalidbutt94@gmail.com>
2020-06-23 20:29:14 +0500

    Add Viewset method in API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
 urlpatterns = [
     url('hello-view/', views.HelloApiView.as_view()),
+    url('', include(router.urls))
 ]
